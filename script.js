c415a438e6eaa5ad759476c800a82cfa607f7038
Rute Souza <rutesouza6801@gmail.com>
2023-10-06 20:30:07 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,7 @@
+// Khadija
+
+function computerPlay() {
+    const choices = ["Rock", "Paper", "Scissors"];
+    const randomIndex = Math.floor(Math.random() * choices.length);
+    return choices[randomIndex];
+}  
