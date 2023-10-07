5afc30a0035df168780d39df39e15b9f411dea5a
Labeodan <labeodanojiromayo@gmail.com>
2023-10-07 01:06:31 +0100

    first commit

--- script.js
+++ script.js
@@ -5,3 +5,20 @@ function computerPlay() {
     const randomIndex = Math.floor(Math.random() * choices.length);
     return choices[randomIndex];
 }  
+
+
+// function playRound
+function playRound(playerSelection, computerSelection) {
+    playerSelection = playerSelection.toLowerCase();
+    if (playerSelection === computerSelection.toLowerCase()) {
+        return "It's a tie!";
+    } else if (
+        (playerSelection === "rock" && computerSelection === "scissors") ||
+        (playerSelection === "paper" && computerSelection === "rock") ||
+        (playerSelection === "scissors" && computerSelection === "paper")
+    ) {
+        return `You Win! ${playerSelection} beats ${computerSelection}`;
+    } else {
+        return `You Lose! ${computerSelection} beats ${playerSelection}`;
+    }
+}
